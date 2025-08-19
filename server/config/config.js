3e97a98f512654e8168cab237c1556be7033d157
GowthamKarthickQA <gautamkarthick220f@gmail.com>
2025-08-19 22:00:44 +0530

    red

--- server/config/config.js
+++ server/config/config.js
@@ -1,38 +1,15 @@
-// import mongoose from 'mongoose'
+import mongoose from 'mongoose'
 
-// const connectToDb = async () => {
-//     try {
-//         mongoose.connection.on("connected", () => { console.log("DB is connected") })
-//         await mongoose.connect(`${process.env.MONGO_URI}`);
-
-
-//     } catch (err) {
-//         console.log("error", err.message)
-
-//     }
-// }
-
-// export default connectToDb
-
-import mongoose from "mongoose";
-
-let isConnected = false;
-
-export const connectDB = async () => {
-    if (isConnected) {
-        return;
-    }
-
+const connectToDb = async () => {
     try {
-        const db = await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        mongoose.connection.on("connected", () => { console.log("DB is connected") })
+        await mongoose.connect(`${process.env.MONGO_URI}`);
 
-        isConnected = db.connections[0].readyState;
-        console.log("✅ MongoDB Connected");
+
     } catch (err) {
-        console.error("❌ MongoDB connection error:", err);
-        throw err;
+        console.log("error", err.message)
+
     }
-};
+}
+
+export default connectToDb
