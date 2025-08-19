22795e1a8156462fb505960f7c4df7fef7c7d3fa
GowthamKarthickQA <gautamkarthick220f@gmail.com>
2025-08-19 22:27:59 +0530

    gdf

--- server/config/config.js
+++ server/config/config.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose'
 const connectToDb = async () => {
     try {
         mongoose.connection.on("connected", () => { console.log("DB is connected") })
-        await mongoose.connect(`${process.env.MONGO_URI}`);
+        await mongoose.connect(`${process.env.MONGO_URI}`, { bufferCommands: 30000 });
 
 
     } catch (err) {
